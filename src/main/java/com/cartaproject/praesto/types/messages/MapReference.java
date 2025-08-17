a3f0c04c899ae9b4e5ab38756e2c575a5c92d1ae
Yuri-IQI <anthony.yuri.ff@gmail.com>
2025-08-17 19:00:47 -0300

    update

--- src/main/java/com/cartaproject/praesto/types/messages/MapReference.java
+++ src/main/java/com/cartaproject/praesto/types/messages/MapReference.java
@@ -1,9 +1,10 @@
 package com.cartaproject.praesto.types.messages;
 
 import com.cartaproject.praesto.types.enums.MapScopeEnum;
+import jakarta.validation.constraints.Null;
 
 public record MapReference(
-        String userReg,
+        @Null String userReg,
         String name,
         MapScopeEnum mapScope,
         String category
